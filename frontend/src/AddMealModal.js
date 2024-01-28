e503b0c8e1ad562652a71d01ef1bef4725c27caf
adrianbozeckiso <adrian.bozecki@spoton.com>
2024-01-28 11:34:08 +0100

    filtering categories

--- frontend/src/AddMealModal.js
+++ frontend/src/AddMealModal.js
@@ -1,9 +1,11 @@
 import styles from './DashboardPage.module.css';
 import React, { useState } from 'react';
 
-function AddMealModal({ onClose, categories, products}) {
+function AddMealModal({ onClose, categories, products, onProductsUpdated }) {
     const [selectedCategories, setSelectedCategories] = useState([]);
     const [selectedProducts, setSelectedProducts] = useState([]);
+    const [newProductName, setNewProductName] = useState('');
+const [newProductUnit, setNewProductUnit] = useState('');
 
   
     const handleCategoryChange = (event) => {
@@ -29,13 +31,49 @@ function AddMealModal({ onClose, categories, products}) {
         setSelectedProducts(value);
       };
 
-
+      const handleAddProduct = async (e) => {
+        e.preventDefault(); // Zapobieganie domyślnej akcji formularza
+      
+        const token = localStorage.getItem('access_token'); // Pobranie tokena
+        const user_id = localStorage.getItem('user_id');
+        const payload = {
+          name: newProductName,
+          unit_of_measure: newProductUnit,
+        };
+      
+        try {
+          const response = await fetch('http://localhost:8000/products', {
+            method: 'POST',
+            headers: {
+              'Authorization': `Bearer ${token}`,
+              'Content-Type': 'application/json',
+              'User_id': user_id,
+            },
+            body: JSON.stringify(payload),
+          });
+      
+          if (response.ok) {
+            alert('Product added successfully!');
+            onProductsUpdated();
+          } else {
+            throw new Error('Failed to add new product');
+          }
+      
+          setNewProductName('');
+          setNewProductUnit('');
+      
+        } catch (error) {
+          console.error('Error adding new product:', error);
+          alert('Failed to add new product.');
+        }
+      };
+      
     
   
     return (
       <div className={styles.modalBackdrop}>
         <div className={styles.modalContent}>
-            
+        
           <h2>Add New Meal</h2>
           <form>
             <input type="text" placeholder="Name" /* reszta atrybutów */ />
@@ -59,6 +97,23 @@ function AddMealModal({ onClose, categories, products}) {
             <button onClick={onClose}>Close</button>
           </form>
         </div>
+        <h3>Add new product</h3>
+        <form onSubmit={handleAddProduct}>
+  <input
+    type="text"
+    placeholder="Product Name"
+    value={newProductName}
+    onChange={(e) => setNewProductName(e.target.value)}
+  />
+  <select value={newProductUnit} onChange={(e) => setNewProductUnit(e.target.value)}>
+    <option value="">Select Unit</option>
+    <option value="GRAM">Gram</option>
+    <option value="MILLILITER">Milliliter</option>
+    <option value="CENTIMETER">Centimeter</option>
+    <option value="PIECE">Piece</option>
+  </select>
+  <button type="submit">Add Product</button>
+</form>
       </div>
     );
   }
--- frontend/src/DashboardPage.js
+++ frontend/src/DashboardPage.js
@@ -5,79 +5,90 @@ import Modal from './Modal';
 import AddMealModal from './AddMealModal';
 
 function DashboardPage() {
-    const navigate = useNavigate();
-    const [categories, setCategories] = useState([]);
-    const [meals, setMeals] = useState([]);
-    const [selectedMeal, setSelectedMeal] = useState(null);
-    const [isModalOpen, setModalOpen] = useState(false);
-    const [isAddMealModalOpen, setAddMealModalOpen] = useState(false);
-    const [products, setProducts] = useState([]);
-  
-    useEffect(() => {
-        const token = localStorage.getItem('access_token');
-        const user_id = localStorage.getItem('user_id');
-        if (!token) {
-          alert('Unauthorized: No token provided');
-          navigate('/');
-        } else {
-          fetch('http://localhost:8000/categories', {
-            headers: {
-              'Authorization': `Bearer ${token}`,
-              'Content-Type': 'application/json',
-              'User_Id': user_id,
-            }
-          })
-          .then(response => response.json())
-          .then(data => {
-            setCategories(data);
-          })
-          .catch(error => {
-            console.error('Error fetching categories', error);
-          });
-        
-          fetch('http://localhost:8000/meals', {
-            headers: {
-              'Authorization': `Bearer ${token}`,
-              'Content-Type': 'application/json',
-              'User_Id': user_id,
-            }
-          })
-          .then(response => response.json())
-          .then(data => {
-            setMeals(data);
-          })
-          .catch(error => {
-            console.error('Error fetching meals', error);
-          });
-          fetch('http://localhost:8000/products', {
-            headers: {
-              'Authorization': `Bearer ${token}`,
-              'Content-Type': 'application/json',
-              'User_Id': user_id,
-            }
-          })
-          .then(response => response.json())
-          .then(data => setProducts(data))
-          .catch(error => console.error('Error fetching products', error));
+  const navigate = useNavigate();
+  const [categories, setCategories] = useState([]);
+  const [meals, setMeals] = useState([]);
+  const [selectedMeal, setSelectedMeal] = useState(null);
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [isAddMealModalOpen, setAddMealModalOpen] = useState(false);
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = () => {
+    const token = localStorage.getItem('access_token');
+    const user_id = localStorage.getItem('user_id');
+    
+    fetch('http://localhost:8000/products', {
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json',
+        'User_Id': user_id,
+      }
+    })
+    .then(response => response.json())
+    .then(data => setProducts(data))
+    .catch(error => console.error('Error fetching products', error));
+  };
+
+  const fetchMeals = (categoryId = null) => {
+    const token = localStorage.getItem('access_token');
+    const user_id = localStorage.getItem('user_id');
+    let url = 'http://localhost:8000/meals';
+
+    if (categoryId) {
+      url += `?category_id=${categoryId}`;
+    }
+
+    fetch(url, {
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json',
+        'User_Id': user_id,
+      }
+    })
+    .then(response => response.json())
+    .then(data => setMeals(data))
+    .catch(error => console.error('Error fetching meals', error));
+  };
+
+  useEffect(() => {
+    const token = localStorage.getItem('access_token');
+    const user_id = localStorage.getItem('user_id');
+    if (!token) {
+      alert('Unauthorized: No token provided');
+      navigate('/');
+    } else {
+      fetch('http://localhost:8000/categories', {
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json',
+          'User_Id': user_id,
         }
-      }, [navigate]);
+      })
+      .then(response => response.json())
+      .then(data => setCategories(data))
+      .catch(error => console.error('Error fetching categories', error));
 
-      const handleMealClick = (meal) => {
-        setSelectedMeal(meal);
-        setModalOpen(true);
-      };
-  
-      const closeModal = () => {
-        setModalOpen(false);
-      };
+      fetchMeals();
+      fetchProducts();
+    }
+  }, [navigate]);
 
-      const openAddMealModal = () => {
-        setAddMealModalOpen(true);
-      };
-  
-      const closeAddMealModal = () => {
-        setAddMealModalOpen(false);
-      };
+  const handleMealClick = (meal) => {
+    setSelectedMeal(meal);
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  const openAddMealModal = () => {
+    setAddMealModalOpen(true);
+  };
+
+  const closeAddMealModal = () => {
+    setAddMealModalOpen(false);
+  };
 
   return (
     <div className={styles.dashboard}>
@@ -87,23 +98,22 @@ function DashboardPage() {
           <button className={styles.addButton} onClick={openAddMealModal}>+ add</button>
           <input className={styles.search} placeholder="search meal" />
           <button className={styles.settingsButton}>logout</button>
-          
         </div>
       </header>
 
       <aside className={styles.sidebar}>
         <nav className={styles.nav}>
-          <button className={styles.navButton}>all</button>
+          <button className={styles.navButton} onClick={() => fetchMeals()}>all</button>
           {categories.map((category) => (
-            <button key={category.id} className={styles.navButton}>
-            {category.name}
-        </button>
+            <button key={category.id} className={styles.navButton} onClick={() => fetchMeals(category.id)}>
+              {category.name}
+            </button>
           ))}
         </nav>
       </aside>
 
       <main className={styles.content}>
-      <section className={styles.meals}>
+        <section className={styles.meals}>
           {meals.map((meal, index) => (
             <div key={index} className={styles.mealCard} onClick={() => handleMealClick(meal)}>
               {meal.name}
@@ -111,12 +121,13 @@ function DashboardPage() {
           ))}
         </section>
       </main>
+
       {isModalOpen && selectedMeal && (
-          <Modal meal={selectedMeal} onClose={closeModal} />
-        )}
+        <Modal meal={selectedMeal} onClose={closeModal} />
+      )}
 
       {isAddMealModalOpen && (
-        <AddMealModal onClose={closeAddMealModal}  categories={categories} products={products}/>
+        <AddMealModal onClose={closeAddMealModal} categories={categories} products={products} onProductsUpdated={fetchProducts}/>
       )}
     </div>
   );
